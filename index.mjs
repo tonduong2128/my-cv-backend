34a49fcaa7067acef1f206d28618ae63208b1d98
Ton Duong <tonduong2128@gmail.com>
2022-05-18 22:48:07 +0700

    up

--- index.mjs
+++ index.mjs
@@ -1,29 +1,29 @@
-import express from 'express'
-import route from "./route/route.mjs"
-import db from './db/db.mjs'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import express from "express";
+import route from "./route/route.mjs";
+import db from "./db/db.mjs";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
 const app = express();
 const port = 3000;
 
 dotenv.config();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 try {
   db.connect();
 } catch (error) {
   app.use("", (req, res) => {
     res.json({ message: 0 });
-  })
+  });
 }
 
-app.use(cors())
+app.use(cors()); //
 
 route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});
