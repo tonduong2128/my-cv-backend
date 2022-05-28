297a2d637d247aee08bc53788abae00cf1a5ac48
Ton Duong <tonduong2128@gmail.com>
2022-05-28 10:42:09 +0700

    update cors

--- db/db.mjs
+++ db/db.mjs
@@ -1,19 +1,19 @@
-import mysql from "mysql2/promise"
+import mysql from "mysql2/promise";
 export default (() => {
-    var connection;
-    return {
-        connect: async () => {
-            connection = await mysql.createPool({
-                host: process.env.HOST,
-                user: process.env.USER,
-                password: process.env.PASSWORD,
-                database: process.env.DATABASE,
-                port: process.env.PORT
-            });
-        },
-        execute: async (query) => {
-            const [rows, fields] = await connection.execute(query);
-            return rows;
-        }
-    }
-})()
+  var connection;
+  return {
+    connect: async () => {
+      connection = await mysql.createPool({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+        port: process.env.PORT,
+      });
+    },
+    execute: async (query) => {
+      const [rows, fields] = await connection.execute(query);
+      return rows;
+    },
+  };
+})();
--- index.mjs
+++ index.mjs
@@ -22,6 +22,15 @@ try {
 
 app.use(cors()); //
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 route(app);
 
 app.listen(port, () => {
